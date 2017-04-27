779b799a8612436b89ac763ff1653ca51e61d92a
philippp <philip.ppp@live.se>
2017-04-27 08:46:38 +0200

    - Uploaded the project to github

---
+++ app/src/main/java/com/example/visiba/visibarectest/Activites/CameraActivity.java
@@ -0,0 +1,15 @@
+package com.example.visiba.visibarectest.Activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.papersoccer.visibarectest.R;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
---
+++ app/src/main/java/com/example/visiba/visibarectest/WallPost.java
@@ -0,0 +1,17 @@
+package com.example.visiba.visibarectest;
+
+import android.media.Image;
+
+import java.util.Date;
+
+/**
+ * Created by Admin on 2017-04-27.
+ */
+
+public class WallPost
+{
+    public String textContent;
+    public Image leftImage;
+    public Image rightImage;
+    public Date postedDate;
+}
