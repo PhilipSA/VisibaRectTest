cc2f80c7c0b0e4a95bbb3922d44ac02a8ae39fbd
philippp <philip.ppp@live.se>
2017-05-01 10:30:43 +0200

    - Made camera_roll_slider_header.xml transparent when collapsed

--- app/src/main/java/com/example/visiba/visibarectest/Activites/CameraActivity.java
+++ app/src/main/java/com/example/visiba/visibarectest/Activites/CameraActivity.java
@@ -6,7 +6,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.example.papersoccer.visibarectest.R;
 import com.example.visiba.visibarectest.AppImage;
@@ -14,7 +16,10 @@ import com.example.visiba.visibarectest.Fragments.Abstractions.IResultReturning;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 public class CameraActivity extends AppCompatActivity implements IResultReturning<AppImage> {
+
     SlidingUpPanelLayout mSlindingUpPanelLayout;
+    ImageView cameraRollSliderHeaderArrow;
+    RelativeLayout cameraRollSliderHeaderLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +27,44 @@ public class CameraActivity extends AppCompatActivity implements IResultReturnin
         setContentView(R.layout.activity_camera);
 
         mSlindingUpPanelLayout = (SlidingUpPanelLayout)findViewById(R.id.sliding_layout);
+        cameraRollSliderHeaderArrow = (ImageView) findViewById(R.id.cameraRollSliderHeaderArrow);
+        cameraRollSliderHeaderLayout = (RelativeLayout) findViewById(R.id.cameraRollSliderHeaderLayout);
 
         mSlindingUpPanelLayout.addPanelSlideListener(new SlidingUpPanelLayout.SimplePanelSlideListener() {
             @Override
             public void onPanelSlide(View panel, float slideOffset) {
+
                 super.onPanelSlide(panel, slideOffset);
             }
 
             @Override
             public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
                 super.onPanelStateChanged(panel, previousState, newState);
+
+                if ((newState == SlidingUpPanelLayout.PanelState.EXPANDED && previousState == SlidingUpPanelLayout.PanelState.DRAGGING) ||
+                        (newState == SlidingUpPanelLayout.PanelState.DRAGGING && previousState == SlidingUpPanelLayout.PanelState.COLLAPSED))
+                {
+                    ((SlidingUpPanelLayout)panel).setOverlayed(false);
+                }
+                else if (newState == SlidingUpPanelLayout.PanelState.COLLAPSED && previousState == SlidingUpPanelLayout.PanelState.DRAGGING)
+                {
+                    ((SlidingUpPanelLayout)panel).setOverlayed(true);
+                }
+                updateCameraRollSliderHeaderLayout(newState);
             }
         });
+    }
+
+    private void updateCameraRollSliderHeaderLayout(SlidingUpPanelLayout.PanelState panelState)
+    {
+        if (panelState == SlidingUpPanelLayout.PanelState.COLLAPSED) {
+            cameraRollSliderHeaderLayout.setBackgroundResource(R.color.transparentColorButton);
+            cameraRollSliderHeaderArrow.setScaleY(1f);
+        }
+        else if (panelState == SlidingUpPanelLayout.PanelState.EXPANDED) {
+            cameraRollSliderHeaderLayout.setBackgroundResource(R.color.colorButton);
+            cameraRollSliderHeaderArrow.setScaleY(-1f);
+        }
     }
 
     @Override
